20482ccdecf85b0c258d2106294cb672113db1dd
hugoroniaG <102172552+hugoroniaG@users.noreply.github.com>
2022-08-02 14:52:38 -0500

    added active admin panels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.create!(email: 'hugo@me.com', password: 'test1234', password_confirmation: 'test1234')
